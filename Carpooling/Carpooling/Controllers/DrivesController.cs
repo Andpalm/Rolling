915891fea0bc804ecf07a37e0ee14ce7ae488659
Andreas Palmblad <p.blad@hotmail.com>
2018-07-22 23:54:06 +0200

    Add and setup Entityframework

---
+++ Carpooling/Carpooling/Controllers/DrivesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Carpooling.Models.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Carpooling.Controllers
+{
+    public class DrivesController : Controller
+    {
+        private TrinityContext context;
+
+        public DrivesController(TrinityContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
